00ed64583225023bbc26de50fa689852494a5cf8
flaigueglia85 <flaigueglia85@gmail.com>
2025-08-12 08:53:24 +0200

    feat: improve checkout layout

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Cart, CartService } from '../../services/cart.service';
 import { ToastService } from '../../services/common/toast.service';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-checkout',
@@ -13,20 +14,27 @@ import { ToastService } from '../../services/common/toast.service';
 })
 export class CheckoutComponent implements OnInit {
   cart: Cart | null = null;
-  customer = { name: '', address: '', email: '' };
+  customer = { email: '' };
+  newsletter = false;
+  acceptTerms = false;
 
   constructor(
     private cartService: CartService,
     private toast: ToastService,
-    private router: Router
+    private router: Router,
+    private auth: AuthService
   ) {}
 
   ngOnInit(): void {
     this.cartService.get().subscribe(c => (this.cart = c));
+    const userInfo = this.auth.getUserInfo();
+    if (userInfo?.email) {
+      this.customer.email = userInfo.email;
+    }
   }
 
   submitOrder(): void {
-    this.cartService.checkout({ customer: this.customer }).subscribe({
+    this.cartService.checkout({ customer: this.customer, newsletter: this.newsletter }).subscribe({
       next: () => {
         this.toast.success('Ordine completato con successo');
         this.router.navigate(['/home']);
